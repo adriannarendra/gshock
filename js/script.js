21365bb8e103d1207adeb3697c0a08902db311cd
adriannarendra <adrianenarendra@example.com>
2023-10-03 09:09:05 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -13,19 +13,19 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll= () => {
-    sections.forEach(sec=>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150 ;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+    // sections.forEach(sec=>{
+    //     let top = window.scrollY;
+    //     let offset = sec.offsetTop - 150 ;
+    //     let height = sec.offsetHeight;
+    //     let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-            });
-        };
-    });
+    //     if(top >= offset && top < offset + height){
+    //         navLinks.forEach(links =>{
+    //             links.classList.remove('active');
+    //             document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+    //         });
+    //     };
+    // });
 
     // remove toggle and navhbar when scroll
     menuIcon.classList.remove('bx-x');
@@ -38,9 +38,9 @@ ScrollReveal({
     reset: true ,
     distance:'80px',
     duration:2000,
-    delay:200
+    delay:200,
 });
 
-ScrollReveal().reveal('.home-content,.heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img,.about-img, .services-container, .portofolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.about-img, .services-container, .models-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.about-content', { origin: 'right' });
